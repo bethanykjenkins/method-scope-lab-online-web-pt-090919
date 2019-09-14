ea00a9aa39c70c311a56cc4e2988c97af97b3952
Bethany Jenkins <bethanykjenkins@rocketmail.com>
2019-09-14 04:11:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrase.rb
+++ lib/catch_phrase.rb
@@ -4,25 +4,3 @@ def mario
 end
 
 p mario
-
-def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
-end
-
-p toadstool
-
-def link 
-  phrase = "It's Dangerous To Go Alone! Take This."
-  puts phrase
-end
-
-link
-
-def any_phrase (phrase)
-  puts phrase
-end
-
-phrase = "Do A Barrell Roll"
-
-p any_phrase (phrase)
